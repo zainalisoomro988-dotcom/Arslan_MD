ddd7d71a4055ebceb14d2b506a7d699de4e0b2ec
ArslanMD Official <numberkliye@gmail.com>
2025-09-27 02:53:39 +0500

    ARSLAN-MD

--- plugins/download.js
+++ plugins/download.js
@@ -305,7 +305,7 @@ cmd({
         document: { url: downloadUrl },
         mimetype: response.data.result.mimetype,
         fileName: response.data.result.fileName,
-        caption: "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ χ нαѕι ❣️*"
+        caption: "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ArslanMD Official❣️*"
       }, { quoted: m });
 
       await conn.sendMessage(from, { react: { text: "✅", key: m.key } });
