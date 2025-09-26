2d97db38d165d6b05635242216f71c6f8f27b737
ArslanMD Official <numberkliye@gmail.com>
2025-09-27 01:44:27 +0500

    ArslanMD Official 🇵🇰

--- config.cjs
+++
@@ -1,3 +0,0 @@
-global.config = global.config || {};
-global.config.VPS_API_BASE = "https://arslan-md-free-vps.onrender.com";
-global.config.OWNER_NUMBER = "923237045919"; // owner ka number without @s.whatsapp.net
