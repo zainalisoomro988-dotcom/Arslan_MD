3cbda432e5e120aeeb2d74d9556a64a1eec26c58
ArslanMD Official <numberkliye@gmail.com>
2025-10-02 01:20:31 +0500

    Create start.js

---
+++ start.js
@@ -0,0 +1,59 @@
+//Created By Arslan-MD Official don't copy without permission of owner
+
+const fs = require('fs');
+const path = require("path");
+const { File } = require("megajs");
+const AdmZip = require("adm-zip");
+const axios = require("axios");
+
+const PLUGINS_DIR = './plugins'; // Directory where plugins will be extracted
+const LIB_DIR = './lib';
+const ZIP_DIR = './'
+const auid = './assets'
+const silsb = './sessions'
+const mydt = './data'
+
+async function downloadAndExtractZip() {
+  try {
+
+  const MEGA_ZIP_LINK = `enter here file link`;
+  
+    if (!fs.existsSync(mydt)) {
+      fs.mkdirSync(mydt, { recursive: true });
+    }
+    if (!fs.existsSync(auid)) {
+      fs.mkdirSync(auid, { recursive: true });
+    }
+    if (!fs.existsSync(silsb)) {
+      fs.mkdirSync(silsb, { recursive: true });
+    }
+    if (!fs.existsSync(PLUGINS_DIR)) {
+      fs.mkdirSync(PLUGINS_DIR, { recursive: true });
+    }
+    if (!fs.existsSync(LIB_DIR)) {
+        fs.mkdirSync(LIB_DIR, { recursive: true });
+      }
+
+    console.log('FETCHING FILE FOR ARSLAN-MD 🚀❤️');
+    
+    const file = File.fromURL(MEGA_ZIP_LINK);
+    const fileData = await file.downloadBuffer();
+
+    const tempZipPath = path.join(__dirname, 'temp.zip');
+    fs.writeFileSync(tempZipPath, fileData);
+    console.log(` ARSLAN-MD FILES DOWNLOADED SUCCESSFULLY ✅`);
+
+    const zip = new AdmZip(tempZipPath);
+    zip.extractAllTo(ZIP_DIR, true); 
+
+    console.log('ARSLAN-MD SUCCESSFULLY CONNECTED ENJOY ❤️✅');
+
+    fs.unlinkSync(tempZipPath);
+    require("./index.js");
+
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+downloadAndExtractZip();
